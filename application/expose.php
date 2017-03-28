17d5d8df0f9b4cba8ce856b15941565fb1dc2e29
Aman Ullah <amanullah.en@gmail.com>
2017-03-29 01:54:15 +0600

    all configured

---
+++ application/expose.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP SimpleWork</title>
+    <link rel="icon" href="image/self/markem-imaje-fav-logo.png" type="image/png" sizes="16x16">
+</head>
+<body>
+    <img  src="image/self/markem-imaje-logo.png" alt="simpleworklogo" style="height: 125px; width: 150px; margin: auto; display: block;">
+    <h1 style="text-align: center;">Welcome to MI9450UQS!</h1>
+</body>
+</html>
